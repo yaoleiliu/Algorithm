636a80188a62bdee73991168f5232d01ae6a0c24
liuyaolei <718229223@qq.com>
2020-12-03 09:31:03 +0800

    20201203

---
+++ 数据结构/基础知识/ArrayList/ListNode.java
@@ -0,0 +1,11 @@
+package ArrayList;
+
+public class ListNode {
+    public int data;
+    ListNode next;
+
+    public ListNode(int data, ListNode next){
+        this.data = data;
+        this.next = next;
+    }
+}
---
+++ 数据结构/基础知识/ArrayList/ListTopic.java
@@ -0,0 +1,12 @@
+package ArrayList;
+
+public class ListTopic {
+
+    /**
+     * 1.初始化一个节点值为num的节点node
+     * 2.如果链表为空，则让num自己形成环形链表，返回即可
+     * 3.令pre=head,curr=head.next，同步移动pre和curr。若遇上pre.val<=node.val且curr.val>=node.val,
+     *   则将node插入到pre和curr之间
+     * 4.如果pre和curr转一圈都没有发现要插入的位置，此时node应插入在头节点前面
+     */
+}
